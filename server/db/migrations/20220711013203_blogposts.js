4b5cd286fe1fcd55337c18066c78c98cfbb20639
Cat Cummings <cummings.catlin@gmail.com>
2022-07-11 13:40:32 +1200

    Added migrations

---
+++ server/db/migrations/20220711013203_blogposts.js
@@ -0,0 +1,13 @@
+exports.up = function(knex) {
+  return knex.scema.createTable('blogposts', function (table) {
+    table.increments('id').primary()
+    table.string('title')
+    table.date('date_created')
+    table.integer('comment_count').defaultsTo(0)
+    table.string('post')
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('blogposts')
+};
