5b5cc12f9c02351c389b3a610ed2988e53aa7d89
Cat Cummings <cummings.catlin@gmail.com>
2022-07-11 16:04:39 +1200

    Get blog posts api works

--- server/db/db.js
+++ server/db/db.js
@@ -2,6 +2,11 @@ const connection = require('./connection')
 
 //db requests
 
+//Get all swim blogs
+function getBlogposts(db = connection) {
+    return db('blogposts').select()
+}
+
 module.exports = {
-    
+    getBlogposts
 }
