ff9c0119ccb354520a7df7d72aee2ce4707668b1
Cat Cummings <cummings.catlin@gmail.com>
2022-07-15 10:00:44 +1200

    comment

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -5,6 +5,7 @@ const Home = () => {
   return (
     <>
       <h1>Welcome to Eliza's Swim Blog!</h1>
+      {/* This is the landing page - I want to have previews/blocks to the other pages */}
     </>
   )
 }
